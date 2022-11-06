55a574dc567588265699b521de0e2e88f1d91a8b
oluwatobiloba kehinde <omotayokehinde11@dmail.com>
2022-11-07 00:31:10 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+
+ 
+function Counter() {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <div className="container">
+        <h2>{count}</h2>
+        <button className="btn" onClick={() => setCount(count + 1)}>
+          INCREMENT
+        </button>
+        <button className="btn" onClick={() => setCount(count - 1)}>
+          DECREMENT
+        </button>
+        <button className="btn-r" onClick={() => setCount(0)}>
+          RESET
+        </button>
+      </div>
+    </>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>React Counter App</h1>
+      <Counter />
     </div>
   );
 }
